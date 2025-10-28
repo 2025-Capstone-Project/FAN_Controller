3756706fcfd8181319f2b7c35fb2e7f5a60c2791
Doo <ghdtnaos@gmail.com>
2025-10-28 11:26:00 +0900

    Update control_sign_test.py
    change ip

--- control_sign_test.py
+++ control_sign_test.py
@@ -1,7 +1,7 @@
 import os, socket, json, random, time
 from contextlib import closing
 
-PI_VPN_IP   = os.getenv("PI_VPN_IP", "192.168.43.4")  # 예시: VPN이 준 Pi의 IP
+PI_VPN_IP   = os.getenv("PI_VPN_IP", "192.168.43.6")  # 예시: VPN이 준 Pi의 IP
 PI_VPN_PORT = int(os.getenv("PI_VPN_PORT", "7000"))
 PERIOD_SEC  = float(os.getenv("PERIOD_SEC", "5"))
 
@@ -22,4 +22,5 @@ def main():
         time.sleep(PERIOD_SEC)
 
 if __name__ == "__main__":
+
     main()
