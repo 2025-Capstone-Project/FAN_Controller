02e97f8aa9b5eaa59805de4e1163cf06d1549b54
Doo <ghdtnaos@gmail.com>
2025-11-22 16:15:37 +0900

    Update process_control_command.py

--- process_control_command.py
+++ process_control_command.py
@@ -62,7 +62,7 @@ async def handle_connection(websocket, path):
             if "gpu_threshold" in data:
                 global_ctl.gpu_thresh = int(data["gpu_threshold"])
 
-            print(f"{global_ctl.mode), {global_ctl.manual_target}, {global_ctl.cpu_thresh}, {global_ctl.gpu_thresh}")
+            print(f"{global_ctl.mode}, {global_ctl.manual_target}, {global_ctl.cpu_thresh}, {global_ctl.gpu_thresh}")
             
             # 2. 현재 상태를 바로 응답 (옵션)
             response = {
@@ -84,6 +84,7 @@ async def main():
 if __name__ == "__main__":
 
     asyncio.run(main())
+
 
 
 
