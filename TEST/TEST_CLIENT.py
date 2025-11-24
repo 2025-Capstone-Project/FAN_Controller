7f709471cc4a3e784b9a86adccbf77b1b644d57f
Doo <ghdtnaos@gmail.com>
2025-11-24 15:04:22 +0900

    TEST CLIENT
    PI 테스트용 클라이언트 파일

---
+++ TEST/TEST_CLIENT.py
@@ -0,0 +1,21 @@
+import socket 
+import json
+import random
+
+# 임의 전송 client 코드
+PI_HOST = '127.0.0.1'
+PI_PORT = '6000'
+
+def send_to_pi(pwm_value: int):
+    payload = json.dumps({"pwm": int(pwm_value)}).encode() # JSON 포맷으로 변경
+    try:
+        with socket.create_connection((PI_HOST, PI_PORT), timeout=2) as s:
+            s.sendall(payload)
+    except Exception as e:
+        print(f"[Network] Pi 전송 실패: {e}")
+
+def main():
+    while True:
+        send_to_pi(random.randint(0,100))
+
+main()
